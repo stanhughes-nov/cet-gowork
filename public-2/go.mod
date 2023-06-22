c234d8e63a7262ddca44a4d8df1e3a274e49f7d9
stanhughes-nov <stan.hughes@nov.com>
2023-06-22 14:07:32 -0500

    Add public-2

---
+++ public-2/go.mod
@@ -0,0 +1,3 @@
+module github.com/stanhughes-nov/cet-gowork/public-2
+
+go 1.20
---
+++ public-2/pub2.go
@@ -0,0 +1,7 @@
+package public2
+
+import "fmt"
+
+func HelloWorld() {
+	fmt.Println("Hello World, from public-2")
+}
